e8d0c91b60ff6f46c909dc9cb679f1a342479879
artem-viktorovich <artemkavasechko@yandex.ru>
2021-10-03 18:56:27 +0300

    17_lesson
    Загрузка нового урока

---
+++ 17_lesson/js/script.js
@@ -0,0 +1,35 @@
+var buttonAddition = document.getElementById('addition');
+var buttonSubtraction = document.getElementById('subtraction');
+var buttonMultiplication = document.getElementById('multiplication');
+var buttonDivision = document.getElementById('division');
+
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
+
+function makeOperation(operationCode){
+var number1 = Number(input1.value);
+var number2 = Number(input2.value);
+	if(operationCode === '+'){
+		var result = number1 + number2;
+	} else if(operationCode === '-'){
+		var result = number1 - number2;
+	} else if(operationCode === '*'){
+		var result = number1 * number2;
+	} else if(operationCode === '/'){
+		var result = number1 / number2;
+} else {
+	window.alert('operation is unknown');
+}
+	window.alert(result);
+}
+
+function onOperationButtonClick(eventObject) {
+	var clickedElement = eventObject.currentTarget;
+	var operation = clickedElement.innerHTML; //получаем элемент с button
+		makeOperation(operation);
+}
+
+buttonAddition.addEventListener('click', onOperationButtonClick);
+buttonSubtraction.addEventListener('click', onOperationButtonClick);
+buttonMultiplication.addEventListener('click', onOperationButtonClick);
+buttonDivision.addEventListener('click', onOperationButtonClick);
