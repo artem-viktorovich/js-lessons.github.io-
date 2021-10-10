7a153db8481285123ca05f6e2b585c6bee5de366
artem-viktorovich <artemkavasechko@yandex.ru>
2021-10-10 12:26:14 +0300

    Object slider
    Переработка слайдера в объект

---
+++ 22_lesson/js/slider.js
@@ -0,0 +1,36 @@
+var slider = {
+	imageUrls: [],
+	showPrevBtn: document.getElementById('prev-show-btn'),
+	nextPrevBtn: document.getElementById('next-show-btn'),
+	slideImage: document.getElementById('slide-image'),   //4 property характеризующие состояние
+	start: function () {
+		this.showPrevBtn.addEventListener('click', this.onShowPrevBtnClick);
+		this.nextPrevBtn.addEventListener('click', this.onNextPrevBtnClick);
+
+		//create images array
+		this.imageUrls.push('https://wallpapersprinted.com/download/1/italdesign_brivido_concept_red_sport_car_hd_cars-HD.jpg');
+		this.imageUrls.push('https://image.winudf.com/v2/image/Y29tLmZhc3QucmFjaW5nLnNwb3J0cy5jYXJzLndhbGxwYXBlcl9zY3JlZW5fMl8xNTM0MDEyMTQ0XzA2OQ/screen-2.jpg?fakeurl=1&type=.jpg');
+		this.imageUrls.push('https://mocah.org/uploads/posts/110064-mansory-4xx-siracusa-geneva-auto-show-2016-sport-car-yellow.jpeg');
+		this.imageUrls.push('https://storge.pic2.me/upload/274/5fa06248876bc2.61604526.jpg');
+
+		// this.slideImage.src = this.imageUrls[this.currentImageIndex];
+		this.showPrevBtn.disabled = true;
+	},
+
+	onShowPrevBtnClick: function(e){
+		this.currentImageIndex--;
+		this.slideImage.src = this.imageUrls[this.currentImageIndex];
+		this.nextPrevBtn.disabled = false;
+		if (this.currentImageIndex === 0){
+			this.showPrevBtn.disabled = true;
+		}
+	},
+	onNextPrevBtnClick: function(e){
+		this.currentImageIndex++;
+		this.slideImage.src = this.imageUrls[this.currentImageIndex]; //ошибка
+		this.showPrevBtn.disabled = false;
+		if (currentImageIndex === (this.imageUrls.length - 1) ){
+			this.nextPrevBtn.disabled = true;
+		} // imageUrls.length - 1 - длина массива
+	}
+};
