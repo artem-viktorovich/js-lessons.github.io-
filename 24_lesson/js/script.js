f4180ed2eecb379134d42ce1c74db1f6140b79f2
artem-viktorovich <artemkavasechko@yandex.ru>
2021-10-10 21:41:50 +0300

    Slider clon
    Создание слайдов не дублируя код

---
+++ 24_lesson/js/script.js
@@ -0,0 +1,7 @@
+var slider1 = slideFactory.CreateNewSlider();
+var slider2 = slideFactory.CreateNewSlider();
+
+slider1.start('itk-slider1');
+slider2.start('itk-slider2');
+
+// slider2.start('itk-slider3');
