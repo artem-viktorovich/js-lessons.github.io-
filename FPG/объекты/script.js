c2d87aadf2580462a37fc6729051279a3743cc94
artem-viktorovich <artemkavasechko@yandex.ru>
2021-12-03 23:40:58 +0300

    Update JS

--- FPG/объекты/script.js
+++ FPG/объекты/script.js
@@ -21,7 +21,7 @@ console.log(userInfo.['likes JS']); //особенность свойства з
 //Вычисляемое либо передаваемое имя
 
 //Вычисляем имя
-
+/* 
 let firstPart = 'likes';
 let userInfo = {
 	name: 'Vasy',
@@ -29,11 +29,42 @@ let userInfo = {
 	[firstPart + ' JS']: true,
 };
 console.log(userInfo['likes JS']); 
-/* Эта функция помогает определить правильность получения имени
+Эта функция помогает определить правильность получения имени
 
 
-*/
+
 
 //Преимущества квадратных скобок
 let key = 'name';
 console.log(userInfo[key]); //с помощью этой функции можно вывести имя
+*/
+/*
+function makeUserInfo (name, age) {
+	return {
+		name: name,
+		age: age,
+	}
+};
+
+let user = makeUserInfo( 'Dfcz', 30); //Своего рода конструктор для рефакторинга, но не конструктор
+console.log(user);
+
+*/
+
+//Изменение объекта, "удаление"
+/*
+let userInfo ={
+	name: "Вася",
+	age: 30,
+	'likes JS': true,
+}
+console.log(userInfo);
+
+delete userInfo.age;
+
+console.log(userInfo);
+
+delete userInfo['likes JS'];
+
+console.log(userInfo);*/
+
