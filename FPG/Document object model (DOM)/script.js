1e7b56c50cd3ec9303dab1a545f00fea35b3e337
artem-viktorovich <artemkavasechko@yandex.ru>
2021-12-26 20:51:57 +0300

    update script

--- FPG/Document object model (DOM)/script.js
+++ FPG/Document object model (DOM)/script.js
@@ -17,7 +17,27 @@ textElement.innerHTML =
 	//содержимое текстового узла/документа
 
 	//Получаем объект
+	/*
 	const textElement = document.querySelector('.content');
-	const qetComment = textElement.nextElementSibling;
+	const qetComment = textElement.nextSibling;
 
 	console.log(qetComment);
+	console.log(qetComment.data);*/
+
+	//Создание элементов и узлов
+
+	//Создание нового элемента (тега)
+
+	const textElement = document.querySelector('.lesson__text');
+
+	const newElement = document.createElement('div'); //создаем новый div
+
+	//Наполняем его контентом
+	newElement.innerHTML = 'Живи, а работай в <span class=\'yellow\'>свободное время</span>';
+	console.log(newElement);
+/*
+	const newText = document.createTextNode('Hello');
+	console.log(newText);
+	*/
+
+	textElement.after(newElement);
