0900f65093b042c2fdf10e33dca74e0711adddea
artem-viktorovich <54232451+artem-viktorovich@users.noreply.github.com>
2022-01-30 14:07:02 +0300

    style management

--- FPG/Document object model (DOM)/script.js
+++ FPG/Document object model (DOM)/script.js
@@ -13,31 +13,35 @@ textElement.innerHTML =
 	console.log(textElement.innerHTML);*/
 
 
-	//data
-	//содержимое текстового узла/документа
+//data
+//содержимое текстового узла/документа
 
-	//Получаем объект
-	/*
-	const textElement = document.querySelector('.content');
-	const qetComment = textElement.nextSibling;
+//Получаем объект
+/*
+const textElement = document.querySelector('.content');
+const qetComment = textElement.nextSibling;
 
-	console.log(qetComment);
-	console.log(qetComment.data);*/
+console.log(qetComment);
+console.log(qetComment.data);*/
 
-	//Создание элементов и узлов
+//Создание элементов и узлов
 
-	//Создание нового элемента (тега)
+//Создание нового элемента (тега)
 
-	const textElement = document.querySelector('.lesson__text');
+// const textElement = document.querySelector('.lesson__text');
 
-	const newElement = document.createElement('div'); //создаем новый div
+// const newElement = document.createElement('div'); //создаем новый div
 
-	//Наполняем его контентом
-	newElement.innerHTML = 'Живи, а работай в <span class=\'yellow\'>свободное время</span>';
-	console.log(newElement);
+//Наполняем его контентом
+// newElement.innerHTML = 'Живи, а работай в <span class=\'yellow\'>свободное время</span>';
+// console.log(newElement);
 /*
 	const newText = document.createTextNode('Hello');
 	console.log(newText);
 	*/
 
-	textElement.after(newElement);
+// textElement.after(newElement);
+
+const element = document.querySelector('.lesson');
+
+element.style.color = "red";
