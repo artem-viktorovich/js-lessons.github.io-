7380538408952b53059f6082a230a3e1bdcc30a9
artem-viktorovich <54232451+artem-viktorovich@users.noreply.github.com>
2022-02-19 15:08:00 +0300

    css

---
+++ Jquery/1_lessons_selectors/js/jquery.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function () {
+	$('.wrapper').css({ 'background': 'black' });
+	$('*').css({ 'border': '2px solid black' });
+	$('.h2').css({ 'border': '2px solid red','color': '#fff' }); // обращение к классу
+	$('#lorem').css({ 'border': '1px solid red', 'color': 'blue' });
+});
