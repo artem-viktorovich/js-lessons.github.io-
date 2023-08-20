96b0c200c26e22087951bb1d6220f1653d122e99
artem-viktorovich <54232451+artem-viktorovich@users.noreply.github.com>
2023-08-20 20:22:12 +0300

    project

---
+++ AlexDudukalo/6_project/script.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let index = 0
+
+// function hello(userName, age){
+//     index++
+//     console.log(index,userName, age);
+// }
+
+// hello("Artem", "17")
+// hello("anton", "19")
+// hello("Nasty", 23)
+// hello("Oleg", "147")
+
+
+//    это позволяет использовать одинаковые переменный, внутри изолированного блока
+// {
+//     let age = 17;
+//     index++
+//     console.log(index,userName, age);
+// }
+
+// {
+//     let age = 19;
+//     console.log(userName, age);
+
+// }
