e25054ae1070e15bc131035c3383bc784791cf4d
artem-viktorovich <54232451+artem-viktorovich@users.noreply.github.com>
2023-08-26 20:31:24 +0300

    web_link

--- AlexDudukalo/7_project/script.js
+++ AlexDudukalo/7_project/script.js
@@ -1,38 +1,108 @@
+//создание элементов
+
+// document.write('<p>Привет, я параграф</p>')
+// document.write('<h1>Привет, я заголовок</h1>')
+// document.write('<ul><li>Элемент 1</li><li>Элемент 2</li></ul>')
+// document.write('<button>я кнопка</button>')
+
+
+// let title = document.createElement("h1")
+// //createElement - созданный объект элемента.
+// title.textContent = 'Я заголовок'
+// //textContent - добавление контента в тег
+// document.body.append(title)
+// //добавление элемента в body
+
+// console.log(title);
+
+
+// let paragraph = document.createElement("p")
+// paragraph.textContent = 'Я параграф'
+// document.body.append(paragraph)
+
+
+// let counter = 0
+// let list = document.createElement("ul")
+
+// let listItem1 = document.createElement("li")
+// counter++
+// listItem1.textContent = `Элемент ${counter}`
+// let listItem2 = document.createElement("li")
+// counter++
+// listItem2.textContent = `Элемент ${counter}`
+// let listItem3 = document.createElement("li")
+// counter++
+// listItem3.textContent = `Элемент ${counter}`
+// list.append(listItem1,listItem2,listItem3)
+// document.body.append(list)
+
+
+//prepend - добавляет элемент в начало 
+
+// document.body.append(list)
+
+//интерактивность
 
 
 
+// let action = document.createElement("button")
+// action.textContent = "Кнопка"
 
+// action.onclick = function(){
+//     title.textContent = "Клик"
+//     title.classList.add('text')
 
+//     let listItem = document.createElement("li")
+//     counter++
+//     listItem.textContent = `Элемент ${counter}`
+//     list.append(listItem)
 
-
-
-
-
-
-
-
-// let index = 0
-
-// function hello(userName, age){
-//     index++
-//     console.log(index,userName, age);
 // }
 
-// hello("Artem", "17")
-// hello("anton", "19")
-// hello("Nasty", 23)
-// hello("Oleg", "147")
+// //onclick - слежка нажатия (слушатель события)
+
+// document.body.append(action)
 
 
-//    это позволяет использовать одинаковые переменный, внутри изолированного блока
-// {
-//     let age = 17;
-//     index++
-//     console.log(index,userName, age);
-// }
+// Программа
+let counter = 0
+let yearThis = 2023
+let divElements = document.createElement("div")
 
-// {
-//     let age = 19;
-//     console.log(userName, age);
+let nameInp = document.createElement("input")
+nameInp.placeholder = "Введите имя"
+nameInp.type = "text"
 
-// }
+let yearInp = document.createElement("input")
+yearInp.placeholder = "Введите  год рождения"
+yearInp.type = "number"
+
+let addBtn = document.createElement("button")
+addBtn.textContent = "Добавить пользователя"
+
+let listUsers = document.createElement("ul")
+
+function getAge(year){
+    return yearThis - year
+}
+
+function createNewUserItem(index, userName, userYear){
+    let itemLi = document.createElement("li")
+    itemLi.textContent = `${index}) ${userName}, год рождения: ${userYear}, возраст ${getAge(userYear)}`
+    return itemLi
+}
+
+addBtn.onclick = function(){
+    counter++
+    let userName = nameInp.value
+    let userYear = Number(yearInp.value)
+    let newItem = createNewUserItem(counter, userName, userYear)
+    nameInp.value = ""
+    yearInp.value = ""
+    listUsers.append(newItem)
+}
+
+
+divElements.append(nameInp, yearInp, addBtn)
+document.body.append(divElements)
+document.body.append(listUsers)
