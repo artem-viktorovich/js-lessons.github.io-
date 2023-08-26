0f0594aeb10714a7bcb338631a5daa8806c45e14
artem-viktorovich <54232451+artem-viktorovich@users.noreply.github.com>
2023-08-26 16:46:14 +0300

    return

--- AlexDudukalo/6_project/script.js
+++ AlexDudukalo/6_project/script.js
@@ -1,38 +1,69 @@
+//расчёт возраста
 
+// let result
 
-
-
-
-
-
-
-
-
-
-
-
-// let index = 0
-
-// function hello(userName, age){
-//     index++
-//     console.log(index,userName, age);
+// function getAge(){
+//     let currentYear = 2023;
+//     let year = 1996;
+//     result = currentYear - year;
 // }
 
-// hello("Artem", "17")
-// hello("anton", "19")
-// hello("Nasty", 23)
-// hello("Oleg", "147")
+// getAge();
+// console.log(result);
 
+//усовершенствуем функцию
 
-//    это позволяет использовать одинаковые переменный, внутри изолированного блока
-// {
-//     let age = 17;
-//     index++
-//     console.log(index,userName, age);
+// function getAge(){
+//     let currentYear = 2023;
+//     let year = 1996;
+//     return currentYear - year;
+// }
+//===========
+// return завершает работу функции
+//==============
+// console.log(getAge());
+
+//выведем переменную в объект
+
+// function getAge(year = 0){
+//     let currentYear = 2023;
+//     return currentYear - year;
 // }
 
-// {
-//     let age = 19;
-//     console.log(userName, age);
+// console.log(getAge(1996));
 
+// function getAge(year = 0){
+//     let currentYear = 2023
+//     return currentYear - year
 // }
+
+// document.write(`Возраст пользователя: ${getAge(1996)}`);
+
+//другой вид зависи через переменную
+
+
+//  let getAge = function (year = 0){
+//     let currentYear = 2023
+//     return currentYear - year 
+// }
+
+// document.write(`Возраст пользователя: ${getAge(1996)}`);
+
+function strong(text) {
+    return `<strong>${text}</strong>` // выводим из вне
+}
+
+
+function printProduct(productName, count, price) {
+    let totalPrice = count * price
+    document.write(`
+        <p>${strong('Наименование товара:')} ${productName}, ${strong('Количество товара:')} ${count},  ${strong('Стоимость товара:')} ${price}р, 
+        ${strong('Общая стоимость:')} ${totalPrice}
+        </p>
+`)
+return totalPrice
+}
+let total = 0
+total = total + printProduct(' Картофель', 2, 230)
+total = total + printProduct(' Яблоки', 2, 600)
+console.log(total);
